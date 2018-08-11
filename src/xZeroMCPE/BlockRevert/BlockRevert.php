2919df910b34c4eb2d73cbb36f0df6c56bac0af4
Andre | xZeroMCPE <andrethegamer911@gmail.com>
2018-08-11 00:17:08 -0400

    Almost forgot about you

--- src/xZeroMCPE/BlockRevert/BlockRevert.php
+++ src/xZeroMCPE/BlockRevert/BlockRevert.php
@@ -13,6 +13,8 @@ use pocketmine\block\Block;
 use pocketmine\block\BlockFactory;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\Item;
 use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\Task;
@@ -32,7 +34,7 @@ class BlockRevert extends PluginBase implements Listener
 
         if (file_exists($this->getDataFolder() . "config.yml")) {
             $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-            if($this->config->exists('Data')){
+            if ($this->config->exists('Data')) {
                 $this->blockTicks = json_decode($this->config->get("Data"), true);
             }
         } else {
@@ -44,17 +46,18 @@ class BlockRevert extends PluginBase implements Listener
         self::$instance = $this;
 
         // You never knew you could do that? But yeah, you can. Making a class file for this would be useless. I'm not you, creating useless files... :P
-        $class = new class extends Task {
+        $class = new class extends Task
+        {
 
             public function onRun(int $currentTick)
             {
-                if(count(BlockRevert::getInstance()->blockTicks) != 0){
-                    foreach (BlockRevert::getInstance()->blockTicks as $index => $data){
-                        if($data['Time'] != 0){
+                if (count(BlockRevert::getInstance()->blockTicks) != 0) {
+                    foreach (BlockRevert::getInstance()->blockTicks as $index => $data) {
+                        if ($data['Time'] != 0) {
                             BlockRevert::getInstance()->blockTicks[$index]['Time']--;
                         } else {
                             $loc = explode(":", $data['Location']);
-                            BlockRevert::getInstance()->getServer()->getLevelByName($data['Level'])->setBlock(new Vector3((int) $loc[0], (int) $loc[1], (int) $loc[2]), BlockFactory::get($data['Block'], $data['Damage']));
+                            BlockRevert::getInstance()->getServer()->getLevelByName($data['Level'])->setBlock(new Vector3((int)$loc[0], (int)$loc[1], (int)$loc[2]), BlockFactory::get($data['Block'], $data['Damage']));
                             unset(BlockRevert::getInstance()->blockTicks[$index]);
                         }
                     }
@@ -66,24 +69,26 @@ class BlockRevert extends PluginBase implements Listener
 
     public function onDisable()
     {
-       if(count($this->blockTicks) != 0) {
-           $this->config->set('Data', json_encode($this->blockTicks, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-       } else {
-           $this->config->remove('Data');
-       }
+        if (count($this->blockTicks) != 0) {
+            $this->config->set('Data', json_encode($this->blockTicks, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        } else {
+            $this->config->remove('Data');
+        }
         $this->config->save();
     }
 
-    public static function getInstance() : BlockRevert {
+    public static function getInstance(): BlockRevert
+    {
         return self::$instance;
     }
 
-    public function onPlace(BlockPlaceEvent $event){
+    public function onPlace(BlockPlaceEvent $event)
+    {
 
         $player = $event->getPlayer();
 
-        if(in_array($player->getLevel()->getName(), $this->config->get("Level to work on"))){
-            if(count($this->config->get('Affected blocks {Empty = all}')) == 0){
+        if (in_array($player->getLevel()->getName(), $this->config->get("Level to work on"))) {
+            if (count($this->config->get('Affected blocks {Empty = all}')) == 0) {
                 $this->blockTicks[] = [
                     "Block" => $event->getBlockReplaced()->getId(),
                     "Damage" => $event->getBlock()->getDamage(),
@@ -92,7 +97,7 @@ class BlockRevert extends PluginBase implements Listener
                     "Time" => $this->config->get("Interval in seconds")
                 ];
             } else {
-                if(in_array($event->getBlock()->getId(), $this->config->get("Affected blocks {Empty = all}"))){
+                if (in_array($event->getBlock()->getId(), $this->config->get("Affected blocks {Empty = all}"))) {
                     $this->blockTicks[] = [
                         "Block" => $event->getBlockReplaced()->getId(),
                         "Damage" => $event->getBlock()->getDamage(),
@@ -101,6 +106,24 @@ class BlockRevert extends PluginBase implements Listener
                         "Time" => $this->config->get("Interval in seconds")
                     ];
                 }
+            }
+        }
+    }
+
+    public function onInteract(PlayerInteractEvent $event)
+    {
+
+        $player = $event->getPlayer();
+
+        if (in_array($player->getLevel()->getName(), $this->config->get("Level to work on"))) {
+            if ($player->getInventory()->getItemInHand()->getId() == Item::BUCKET) {
+                $this->blockTicks[] = [
+                    "Block" => Block::get(0),
+                    "Damage" => $event->getBlock()->getDamage(),
+                    "Location" => $event->getTouchVector()->getX() . ":" . $event->getTouchVector()->getY() . ":" . $event->getTouchVector()->getZ(),
+                    "Level" => $event->getBlock()->getLevel()->getName(),
+                    "Time" => $this->config->get("Interval in seconds")
+                ];
             }
         }
     }
